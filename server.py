439af979279fd729947dfd44e7f7a9ebf2fac2f2
Suerwin <Suerwin35@gmail.com>
2026-03-30 07:33:24 +0700

    Add MongoDB client import to server.py

--- server.py
+++ server.py
@@ -1,3 +1,5 @@
+from pymongo import MongoClient
+import os
 from fastapi import FastAPI, APIRouter, HTTPException, Depends, status
 from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 from fastapi.responses import StreamingResponse
